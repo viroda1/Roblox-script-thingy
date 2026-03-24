d490e049d6be4a79836b4b3337445b1e837c122a
viroda1 <blockblastsrants777@gmail.com>
2026-03-24 07:37:31 -0400

    Delete mob.lua

--- mob.lua
+++
@@ -1,9 +0,0 @@
-local success, result = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/viroda1/Roblox-script-thingy/refs/heads/main/Mob.lua"))()
-end)
-
-if success then
-    print("HAVE FUN AND REMEMBER TO SHARE WITH UR FRIENDS.")
-else
-    warn("FUCK IT DIDNT WORK.")
-end
